7558448c78116dcb65d8e135f6b87e6ad78095f0
zylxr <zhangyong@live.com>
2024-07-29 22:19:07 +0800

    Revert "save to git repository"
    This reverts commit 6d9fc271afe2f945ab2e78ec4a59f27a0740f140.

--- startsample/components/nested-layout.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function NestedLayout({ children }) {
-    return (
-      <>
-        <main>Nested Layout:<br /> {children} </main>
-      </>
-    )
-  }
--- startsample/pages/test/test1/test2.tsx
+++
@@ -1,3 +0,0 @@
-export default function test2(){
-    return <h1>Hello, test2</h1>
-}
