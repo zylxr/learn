85000861474f4b701f3f389e6f78eb272a343d2e
zylxr <zhangyong@live.com>
2024-08-15 22:23:32 +0800

    update on 20240815 daily excercise

--- Algorithm/dp/FindNumberOfLISClass.cs
+++ Algorithm/dp/FindNumberOfLISClass.cs
@@ -27,7 +27,7 @@ namespace Algorithm.dp
 
         //1 <= nums.length <= 2000
         //-106 <= nums[i] <= 106
-        public int FindNumberOfLIS(int[] nums)
+        public int FindNumberOfLIS1(int[] nums)
         {
             var n = nums.Length;
             var dp = new int[n];
@@ -69,6 +69,78 @@ namespace Algorithm.dp
                     countNum += counts[i];
             }
             return countNum;
+        }
+
+        public int FindNumberOfLIS(int[] nums)
+        {
+            IList<IList<int>> d = new List<IList<int>>();
+            IList<IList<int>> cnt = new List<IList<int>>();
+            foreach (int v in nums)
+            {
+                int i = BinarySearch1(d.Count, d, v);
+                int c = 1;
+                if (i > 0)
+                {
+                    int k = BinarySearch2(d[i - 1].Count, d[i - 1], v);
+                    c = cnt[i - 1][cnt[i - 1].Count - 1] - cnt[i - 1][k];
+                }
+                if (i == d.Count)
+                {
+                    IList<int> dIList = new List<int>();
+                    dIList.Add(v);
+                    d.Add(dIList);
+                    IList<int> cntIList = new List<int>();
+                    cntIList.Add(0);
+                    cntIList.Add(c);
+                    cnt.Add(cntIList);
+                }
+                else
+                {
+                    d[i].Add(v);
+                    int cntSize = cnt[i].Count;
+                    cnt[i].Add(cnt[i][cntSize - 1] + c);
+                }
+            }
+
+            int count1 = cnt.Count, count2 = cnt[count1 - 1].Count;
+            return cnt[count1 - 1][count2 - 1];
+        }
+
+        public int BinarySearch1(int n, IList<IList<int>> d, int target)
+        {
+            int l = 0, r = n;
+            while (l < r)
+            {
+                int mid = (l + r) / 2;
+                IList<int> list = d[mid];
+                if (list[list.Count - 1] >= target)
+                {
+                    r = mid;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return l;
+        }
+
+        public int BinarySearch2(int n, IList<int> list, int target)
+        {
+            int l = 0, r = n;
+            while (l < r)
+            {
+                int mid = (l + r) / 2;
+                if (list[mid] < target)
+                {
+                    r = mid;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return l;
         }
 
     }
