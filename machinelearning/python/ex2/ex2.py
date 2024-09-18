82df86f33db48e41497b28c2024a60a7c95b2f8f
zylxr <zhangyong@live.com>
2024-09-19 01:14:59 +0800

    ex2 python excercise

---
+++ machinelearning/python/ex2/ex2.py
@@ -0,0 +1,279 @@
+# 备注
+#现在我知道我应该考虑列向量，而Tensorflow对数据的形状非常挑剔。 但是在numpy中，正常的一维ndarray已经被表示为列向量。
+#如果我重新塑造$\mathbb{R}^n$ 为 $\mathbb{R}^{n\times1}$，它不再是列向量了，而是是1列的矩阵,那使用scipy会有麻烦。
+#所以我们应该把TensorFlow的数据视为特殊情况。 我们继续使用numpy的惯例。
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+import seaborn as sns
+
+data = pd.read_csv('ex2data1.txt', names=['exam1', 'exam2', 'admitted'])
+data.head()#看前五行
+data.describe()
+
+sns.set(context="notebook", style="darkgrid", palette=sns.color_palette("RdBu", 2))
+
+sns.lmplot(x='exam1', y='exam2', hue='admitted', data=data, 
+           fit_reg=False, 
+           scatter_kws={"s": 50}
+          )
+plt.show()#看下数据的样子
+
+def get_X(df):#读取特征
+#     """
+#     use concat to add intersect feature to avoid side effect
+#     not efficient for big dataset though
+#     """
+    ones = pd.DataFrame({'ones': np.ones(len(df))})#ones是m行1列的dataframe
+    data = pd.concat([ones, df], axis=1)  # 合并数据，根据列合并
+    return data.iloc[:, :-1]  # 这个操作返回 ndarray,不是矩阵
+
+
+def get_y(df):#读取标签
+#     '''assume the last column is the target'''
+    return np.array(df.iloc[:, -1])#df.iloc[:, -1]是指df的最后一列
+
+
+def normalize_feature(df):
+#     """Applies function along input axis(default 0) of DataFrame."""
+    return df.apply(lambda column: (column - column.mean()) / column.std())#特征缩放
+
+X = get_X(data)
+print(X.shape)
+
+y = get_y(data)
+print(y.shape)
+
+# sigmoid 函数
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(np.arange(-10, 10, step=0.01),
+        sigmoid(np.arange(-10, 10, step=0.01)))
+ax.set_ylim((-0.1,1.1))
+ax.set_xlabel('z', fontsize=18)
+ax.set_ylabel('g(z)', fontsize=18)
+ax.set_title('sigmoid function', fontsize=18)
+plt.show()
+
+# cost function(代价函数)
+theta=np.zeros(3) # X(m*n) so theta is n*1
+
+def cost(theta, X, y):
+    ''' cost fn is -l(theta) for you to minimize'''
+    return np.mean(-y * np.log(sigmoid(X @ theta)) - (1 - y) * np.log(1 - sigmoid(X @ theta)))
+
+# X @ theta与X.dot(theta)等价
+
+# gradient descent(梯度下降)
+def gradient(theta, X, y):
+#     '''just 1 batch gradient'''
+    return (1 / len(X)) * X.T @ (sigmoid(X @ theta) - y)
+
+gradient(theta, X, y)
+
+# 拟合参数
+# * 这里我使用 [`scipy.optimize.minimize`](http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize) 去寻找参数  
+
+import scipy.optimize as opt
+res = opt.minimize(fun=cost, x0=theta, args=(X, y), method='Newton-CG', jac=gradient)
+
+print(res)
+
+# 用训练集预测和验证
+
+def predict(x, theta):
+    prob = sigmoid(x @ theta)
+    return (prob >= 0.5).astype(int)
+
+final_theta = res.x
+y_pred = predict(X, final_theta)
+
+print(classification_report(y, y_pred))
+
+# 寻找决策边界
+#http://stats.stackexchange.com/questions/93569/why-is-logistic-regression-a-linear-classifier
+#> $X \times \theta = 0$  (this is the line)
+print(res.x) # this is final theta
+coef = -(res.x / res.x[2])  # find the equation
+print(coef)
+
+x = np.arange(130, step=0.1)
+y = coef[0] + coef[1]*x
+
+data.describe()  # find the range of x and y
+
+sns.set(context="notebook", style="ticks", font_scale=1.5)
+
+sns.lmplot(x='exam1', y='exam2', hue='admitted', data=data, 
+           fit_reg=False, 
+           scatter_kws={"s": 25}
+          )
+
+plt.plot(x, y, 'grey')
+plt.xlim(0, 130)
+plt.ylim(0, 130)
+plt.title('Decision Boundary')
+plt.show()
+
+# 3- 正则化逻辑回归
+
+df = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
+df.head()
+
+sns.set(context="notebook", style="ticks", font_scale=1.5)
+
+sns.lmplot(x='test1', y='test2', hue='accepted', data=df, 
+           fit_reg=False, 
+           scatter_kws={"s": 50}
+          )
+
+plt.title('Regularized Logistic Regression')
+plt.show()
+
+# # feature mapping（特征映射）
+
+# polynomial expansion
+
+# ```
+# for i in 0..i
+#   for p in 0..i:
+#     output x^(i-p) * y^p
+# ```
+
+def feature_mapping(x, y, power, as_ndarray=False):
+#     """return mapped features as ndarray or dataframe"""
+    # data = {}
+    # # inclusive
+    # for i in np.arange(power + 1):
+    #     for p in np.arange(i + 1):
+    #         data["f{}{}".format(i - p, p)] = np.power(x, i - p) * np.power(y, p)
+
+    data = {"f{}{}".format(i - p, p): np.power(x, i - p) * np.power(y, p)
+                for i in np.arange(power + 1)
+                for p in np.arange(i + 1)
+            }
+
+    if as_ndarray:
+        return pd.DataFrame(data).to_numpy()
+    else:
+        return pd.DataFrame(data)
+    
+
+x1 = np.array(df.test1)
+x2 = np.array(df.test2)
+data = feature_mapping(x1, x2, power=6)
+print(data.shape)
+data.head()
+
+data.describe()
+
+theta = np.zeros(data.shape[1])
+X = feature_mapping(x1, x2, power=6, as_ndarray=True)
+print(X.shape)
+
+y = get_y(df)
+print(y.shape)
+
+#正则化代价函数
+def regularized_cost(theta, X, y, l=1):
+#     '''you don't penalize theta_0'''
+    theta_j1_to_n = theta[1:]
+    regularized_term = (l / (2 * len(X))) * np.power(theta_j1_to_n, 2).sum()
+
+    return cost(theta, X, y) + regularized_term
+#正则化代价函数
+
+regularized_cost(theta, X, y, l=1)
+
+# regularized gradient(正则化梯度)
+def regularized_gradient(theta, X, y, l=1):
+#     '''still, leave theta_0 alone'''
+    theta_j1_to_n = theta[1:]
+    regularized_theta = (l / len(X)) * theta_j1_to_n
+
+    # by doing this, no offset is on theta_0
+    regularized_term = np.concatenate([np.array([0]), regularized_theta])
+
+    return gradient(theta, X, y) + regularized_term
+
+regularized_gradient(theta, X, y)
+
+#拟合参数
+import scipy.optimize as opt
+
+print('init cost = {}'.format(regularized_cost(theta, X, y)))
+
+res = opt.minimize(fun=regularized_cost, x0=theta, args=(X, y), method='Newton-CG', jac=regularized_gradient)
+
+#预测
+final_theta = res.x
+y_pred = predict(X, final_theta)
+
+print(classification_report(y, y_pred))
+
+def draw_boundary(power, l):
+#     """
+#     power: polynomial power for mapped feature
+#     l: lambda constant
+#     """
+    density = 1000
+    threshhold = 2 * 10**-3
+
+    final_theta = feature_mapped_logistic_regression(power, l)
+    x, y = find_decision_boundary(density, power, final_theta, threshhold)
+
+    df = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
+    sns.lmplot(x='test1', y='test2', hue='accepted', data=df, fit_reg=False, scatter_kws={"s": 100})
+
+    plt.scatter(x=x, y=y, c='r', s=10)
+    plt.title('Decision boundary')
+    plt.show()
+
+
+def feature_mapped_logistic_regression(power, l):
+#     """for drawing purpose only.. not a well generealize logistic regression
+#     power: int
+#         raise x1, x2 to polynomial power
+#     l: int
+#         lambda constant for regularization term
+#     """
+    df = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
+    x1 = np.array(df.test1)
+    x2 = np.array(df.test2)
+    y = get_y(df)
+
+    X = feature_mapping(x1, x2, power, as_ndarray=True)
+    theta = np.zeros(X.shape[1])
+
+    res = opt.minimize(fun=regularized_cost,
+                       x0=theta,
+                       args=(X, y, l),
+                       method='TNC',
+                       jac=regularized_gradient)
+    final_theta = res.x
+
+    return final_theta
+
+def find_decision_boundary(density, power, theta, threshhold):
+    t1 = np.linspace(-1, 1.5, density)
+    t2 = np.linspace(-1, 1.5, density)
+
+    cordinates = [(x, y) for x in t1 for y in t2]
+    x_cord, y_cord = zip(*cordinates)
+    mapped_cord = feature_mapping(x_cord, y_cord, power)  # this is a dataframe
+
+    inner_product = mapped_cord.to_numpy() @ theta
+
+    decision = mapped_cord[np.abs(inner_product) < threshhold]
+
+    return decision.f10, decision.f01
+#寻找决策边界函数
+
+draw_boundary(power=6, l=1)#lambda=1
+
+draw_boundary(power=6, l=0)  # no regularization, over fitting，#lambda=0,没有正则化，过拟合了
+
+draw_boundary(power=6, l=100)  # underfitting，#lambda=100,欠拟合
