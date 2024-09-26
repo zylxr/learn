d748fbb35eccecd6158b014c3c7203cea8e494dc
zylxr <zhangyong@live.com>
2024-09-27 01:51:16 +0800

    ex4 octave code

--- machinelearning/octave_code/ex4/checkNNGradients.m
+++ machinelearning/octave_code/ex4/checkNNGradients.m
@@ -28,9 +28,11 @@ y  = 1 + mod(1:m, num_labels)';
 nn_params = [Theta1(:) ; Theta2(:)];
 
 % Short hand for cost function
-costFunc = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, ...
-                               num_labels, X, y, lambda);
+%costFunc = @(p) nnCostFunction(p, input_layer_size, hidden_layer_size, ...
+%                               num_labels, X, y, lambda);
 
+costFunc = @(p) nnCostFunction(p,input_layer_size,hidden_layer_size,...
+                                num_labels,X,y,lambda);
 [cost, grad] = costFunc(nn_params);
 numgrad = computeNumericalGradient(costFunc, nn_params);
 
