9ba7f64b454c6414e2edd7875d1e97be89cc1f48
zylxr <zhangyong@live.com>
2024-10-04 12:00:29 +0800

    20241004 daily excercise

--- Algorithm/DailyExcise/202410/RunOctoberDailyExcercise.cs
+++ Algorithm/DailyExcise/202410/RunOctoberDailyExcercise.cs
@@ -26,6 +26,33 @@ namespace Algorithm.DailyExcise
 
             minSpeedOnTimeResult = minSpeedOnTimeClass.MinSpeedOnTime(new int[] { 37, 64, 81 }, 3.11);//73
 
+            var minCostClass = new MinCostClass();
+            var minCostResult = minCostClass.MinCost(30,new int[][] { 
+                new int[]{ 0,1,10},
+                new int[]{ 1,2,10}, 
+                new int[]{2,5,10},
+                new int[]{0,3,1 },
+                new int[]{3,4,10},
+                new int[]{4,5,15}
+            }, new int[] { 5, 1, 2, 20, 20, 3 });//11
+
+
+            minCostResult = minCostClass.MinCost(100, new int[][] {
+                new int[]{ 0,1,100}
+            }, new int[] { 2,5 });//7
+
+
+            minCostResult = minCostClass.MinCost2(30, new int[][] {
+                new int[]{ 0,1,10},
+                new int[]{ 1,2,10},
+                new int[]{2,5,10},
+                new int[]{0,3,1 },
+                new int[]{3,4,10},
+                new int[]{4,5,15}
+            }, new int[] { 5, 1, 2, 20, 20, 3 });//11
+
+            var nthPersonGetNthSeatClass = new NthPersonGetsNthSeatClass();
+            var nthPersonGetNthSeatResult = nthPersonGetNthSeatClass.NthPersonGetsNthSeat(4);//0.5
         }
     }
 }
