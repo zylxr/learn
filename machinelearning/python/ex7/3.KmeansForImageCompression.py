f90bbdb5ddc8533be98a17b6832ac4f39b00b988
zylxr <zhangyong@live.com>
2024-10-16 12:44:55 +0800

    add excercise pdf

--- machinelearning/python/ex7/3.KmeansForImageCompression.py
+++ machinelearning/python/ex7/3.KmeansForImageCompression.py
@@ -40,30 +40,37 @@ ax[0].imshow(pic)
 ax[1].imshow(compressed_pic)
 plt.show()
 
+# 绘图前的准备 - 随机采样 1000 个数据点
+sample_size = 1000
+np.random.seed(42)  # 设置随机种子以保证结果可复现
+indices = np.random.choice(np.arange(data.shape[0]), size=sample_size, replace=False)
+sample_data = data[indices]
+sample_C = C[indices]
+
 #绘图 3D & 2D
 K=16
 # Setup Color Palette
 palette = plt.get_cmap('hsv')(np.linspace(0, 1, K))
-colors = palette[C]
+colors = palette[sample_C]
 
 # Visualize the data and centroid memberships in 3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=colors, s=10)
+ax.scatter(sample_data[:, 0], sample_data[:, 1], sample_data[:, 2], c=colors, s=10)
 ax.set_title('Pixel dataset plotted in 3D. Color shows centroid memberships')
 plt.show()
 
 # Plot in 2D
 # Subtract the mean to use PCA
 scaler = StandardScaler()
-X_norm = scaler.fit_transform(data)
+X_norm = scaler.fit_transform(sample_data)
 
 # PCA and project the data to 2D
 pca = PCA(n_components=2)
 Z = pca.fit_transform(X_norm)
 plt.figure()
 for i in range(K):
-    plt.scatter(Z[(C == i), 0], Z[(C == i), 1], color=palette[i], label=f'Centroid {i}')
+    plt.scatter(Z[(sample_C== i), 0], Z[(sample_C == i), 1], color=palette[i], label=f'Centroid {i}')
 plt.legend()
 plt.title('PCA Projection of Pixel Dataset to 2D')
 plt.show()
