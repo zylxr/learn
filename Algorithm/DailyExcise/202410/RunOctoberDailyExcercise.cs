de80e6f5008e015b203bb9d7b08bef0b866baef0
zylxr <zhangyong@live.com>
2024-10-23 12:16:49 +0800

    20241023 daily excercise

--- Algorithm/DailyExcise/202410/RunOctoberDailyExcercise.cs
+++ Algorithm/DailyExcise/202410/RunOctoberDailyExcercise.cs
@@ -126,6 +126,9 @@ namespace Algorithm.DailyExcise
 
             var smallestRangeIIClass = new SmallestRangeIIClass();
             var smallestRangeIIResult = smallestRangeIIClass.SmallestRangeII(new int[] { 0, 10 },2); //6
+
+            var countCompleteDaysPaisClass = new CountCompleteDayPairsClass();
+            var countCompleteDaysPairResult = countCompleteDaysPaisClass.CountCompleteDayPairs(new int[] { 12, 12, 30, 24, 24 });//2
         }
     }
 }
