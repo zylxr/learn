d41c7b04a89d4a9dd8c9fa4bd524846036473c9f
zylxr <zhangyong@live.com>
2024-12-24 14:11:52 +0800

    20241224 daily excercise

--- Algorithm/DailyExcise/202412/RunDecember24.cs
+++ Algorithm/DailyExcise/202412/RunDecember24.cs
@@ -66,6 +66,9 @@ namespace Algorithm.DailyExcise
             examRoomClass.Leave(4);
             examRoomClass.Seat();//5
 
+            var eatenAppleClass = new EatenApplesClass();
+            var eatenAppleResult = eatenAppleClass.EatenApples(new int[] { 1, 2, 3, 5, 2 },new int[]{ 3, 2, 1, 4, 2 });//7
+
         }
     }
 }
