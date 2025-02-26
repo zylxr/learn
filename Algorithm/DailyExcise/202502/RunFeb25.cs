2b4171eb3e4c7c572f9ef0504f234994138fe634
zylxr <zhangyong@live.com>
2025-02-26 12:39:22 +0800

    20250226 daily excercise

--- Algorithm/DailyExcise/202502/RunFeb25.cs
+++ Algorithm/DailyExcise/202502/RunFeb25.cs
@@ -112,6 +112,19 @@ namespace Algorithm.DailyExcise
             allocatorResult = allocatorClass.Allocate(3, 4);//3
             allocatorResult = allocatorClass.Allocate(1, 1);//1
             allocatorResult = allocatorClass.Allocate(1, 1);//6
+
+            var browserHistory = new BrowserHistoryClass("leetcode.com");
+            browserHistory.Visit("google.com");//你原本在浏览 "leetcode.com" 。访问 "google.com"
+            browserHistory.Visit("facebook.com");     // 你原本在浏览 "google.com" 。访问 "facebook.com"
+            browserHistory.Visit("youtube.com");      // 你原本在浏览 "facebook.com" 。访问 "youtube.com"
+            var browserHistoryResult = browserHistory.Back(1);                   // 你原本在浏览 "youtube.com" ，后退到 "facebook.com" 并返回 "facebook.com"
+
+            browserHistoryResult = browserHistory.Back(1);                   // 你原本在浏览 "facebook.com" ，后退到 "google.com" 并返回 "google.com"
+            browserHistoryResult = browserHistory.Forward(1);                // 你原本在浏览 "google.com" ，前进到 "facebook.com" 并返回 "facebook.com"
+            browserHistory.Visit("linkedin.com");     // 你原本在浏览 "facebook.com" 。 访问 "linkedin.com"
+            browserHistoryResult = browserHistory.Forward(2);                // 你原本在浏览 "linkedin.com" ，你无法前进任何步数。
+            browserHistoryResult = browserHistory.Back(2);                   // 你原本在浏览 "linkedin.com" ，后退两步依次先到 "facebook.com" ，然后到 "google.com" ，并返回 "google.com"
+            browserHistoryResult = browserHistory.Back(7);                   // 你原本在浏览 "google.com"， 你只能后退一步到 "leetcode.com" ，并返回 "leetcode.com"
         }
     }
 }
