4197c06450fc78011c8e6af07d3e3e1af3887447
zylxr <zhangyong@live.com>
2025-07-04 01:39:00 +0800

    20250704 daily excercise

--- Algorithm/DailyExcise/2025/202507/RunJuly25.cs
+++ Algorithm/DailyExcise/2025/202507/RunJuly25.cs
@@ -12,6 +12,10 @@ namespace Algorithm.DailyExcise
         {
             var possibleStringCountClass = new PossibleStringCountClass();
             var possibleStringCountResult = possibleStringCountClass.PossibleStringCount("aabbccdd",7);//5
+
+            var kthCharacterClass = new KthCharacterClass();
+            var kthCharacterClassResult = kthCharacterClass.KthCharacter(2, new int[] { 0 });//a
+            kthCharacterClassResult = kthCharacterClass.KthCharacter(3, new int[] { 1,0 });//a
         }
     }
 }
