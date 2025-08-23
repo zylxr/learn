6543de62c7b0a8c35ff7b793e5fe54a8c5ff6d44
zylxr <zhangyong@live.com>
2025-08-23 13:56:39 +0800

    20250823 daily excercise

--- Algorithm/DailyExcise/2025/202508/Run0825.cs
+++ Algorithm/DailyExcise/2025/202508/Run0825.cs
@@ -98,6 +98,13 @@ namespace Algorithm.DailyExcise
                 new int[] { 0, 0,0 },
                 new int[] { 0, 0,1 }
             });//1
+
+            var minimumSumClass = new MinimumSumClass();
+            var minimumSumClassResult = minimumSumClass.MinimumSum(
+                new int[][] { 
+                    new int[] { 1, 0, 1 },
+                    new int[] { 1, 1,1 }
+                });//5
         }
     }
 }
