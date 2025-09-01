b795e324f80e6b0b3ba370e5323b1493ee8b975d
zylxr <zhangyong@live.com>
2025-09-02 03:23:05 +0800

    20250902 daily excercise

--- Algorithm/DailyExcise/2025/202509/RunSep25.cs
+++ Algorithm/DailyExcise/2025/202509/RunSep25.cs
@@ -40,6 +40,31 @@ namespace Algorithm.DailyExcise
             //    new int[] { 4, 5 },
             //    new int[] { 2, 10 }
             //}, 4);//0.53485
+
+            var numberOfPairsClass2 = new NumberOfPairsClass2();
+            var numberOfPairsClass2Result = numberOfPairsClass2.NumberOfPairs(
+                new int[][] {
+                    new int[]{ 0,1},
+                    new int[]{1,3 },
+                    new int[]{6,1}
+                }
+                );//2
+
+            numberOfPairsClass2Result = numberOfPairsClass2.NumberOfPairs(
+                new int[][] {
+                    new int[]{ 0,2},
+                    new int[]{5,1 },
+                    new int[]{5,6}
+                }
+                );//2
+
+            numberOfPairsClass2Result = numberOfPairsClass2.NumberOfPairs(
+                new int[][] {
+                    new int[]{ 6,2},
+                    new int[]{4,4 },
+                    new int[]{2,6}
+                }
+                );//2
         }
     
     }
