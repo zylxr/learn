db79613612b8afeace10662944d90532e80d4d8f
zylxr <zhangyong@live.com>
2026-03-13 23:01:21 +0800

    20260313 daily excercise

--- Algorithm/DailyExcise/2026/202603/RunMarch26.cs
+++ Algorithm/DailyExcise/2026/202603/RunMarch26.cs
@@ -23,6 +23,9 @@ namespace Algorithm.DailyExcise
             var minFlipsClass3 = new MinFlipsClass3();
             var minFlipsClass3Result = minFlipsClass3.MinFlips("111000");//2
             minFlipsClass3Result = minFlipsClass3.MinFlips2("111000");//2
+
+            var minNumberOfSeconds = new MinNumberOfSecondsClass();
+            var minNumberOfSecondsResult = minNumberOfSeconds.MinNumberOfSeconds(4,new int[] {2,1,1 });//3
         }
     }
 }
